67041912a80d5544f3e061e56521c12132d3404f
Terra Manning <terradmanning@gmail.com>
2024-11-24 18:32:50 -0700

    feat: change the attributes to get correct response from upsplash

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -10,18 +10,31 @@ class Api::V1::ImagesController < ApplicationController
     response = conn.get("/search/photos", { query: artist })
     
     json = JSON.parse(response.body, symbolize_names: true)
-    first_photo = json[:results][0]
+    # first_photo = json[:results][0]
 
-    formatted_json = {
-      id: nil, 
-      type: "image", 
-      attributes: {
-        image_url: first_photo[:url],
-        photographer: first_photo[:photographer],
-        photographer_url: first_photo[:photographer_url],
-        alt_text: first_photo[:alt]
-      }
-    }
+    # formatted_json = {
+    #   id: nil, 
+    #   type: "image", 
+    #   attributes: {
+    #     image_url: first_photo[:url],
+    #     photographer: first_photo[:photographer],
+    #     photographer_url: first_photo[:photographer_url],
+    #     alt_text: first_photo[:alt]
+    #   }
+    # }
+
+  first_photo = json[:results][0]
+
+  formatted_json = {
+  id: first_photo[:id],
+  type: "image",
+  attributes: {
+    image_url: first_photo[:urls][:regular],
+    photographer: first_photo[:user][:name],
+    photographer_url: first_photo[:user][:links][:html],
+    alt_text: first_photo[:alt_description]
+  }
+}
     render json: { data: formatted_json }
   end
 end
